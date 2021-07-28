de976bcebbc7f993a51820ba9085a219f44c19ad
Stetson Harding <stetson2001@msn.com>
2021-07-27 20:08:27 -0700

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import Search from "../src/components/search/Search";
-
-<<<<<<< HEAD
-function App() {
-  return <Search />;
-=======
 import Search from "./components/search/Search";
-import "./app.css"
+import "./app.css";
 
-export default function App() {
-
- 
-
+function App() {
   return (
     <>
       <Search />
     </>
   );
->>>>>>> 6175e33900a95d1776166329746e78f9bce72bfe
 }
-
 export default App;
